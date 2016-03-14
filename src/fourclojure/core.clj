54ec764e485b9a4ec108978899994aec87e465e0
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-03-14 15:55:20 +0000

    Q43: Reverse Interleave working.

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -3,3 +3,13 @@
 (defn my-flatten
   [l]
   (filter (complement sequential?) (tree-seq sequential? seq l)))
+
+(defn reverse-interleave
+  [l n]
+  (apply map list
+        (loop
+          [l' l
+           acc (list l)]
+          (if (empty? (drop n l'))
+            (reverse acc)
+            (recur (drop n l') (cons (drop n l') acc))))))
--- test/fourclojure/core_test.clj
+++ test/fourclojure/core_test.clj
@@ -14,3 +14,19 @@
     (my-flatten ["a" ["b"] "c"]))))
   (testing "symbol"
     (is (= '(:a) (my-flatten '((((:a)))))))))
+
+
+(deftest q43
+  "Reverse interleave"
+  (testing "vector"
+    (is (=
+      '((1 3 5) (2 4 6))
+      (reverse-interleave [1 2 3 4 5 6] 2))))
+  (testing "range"
+    (is (=
+      '((0 3 6) (1 4 7) (2 5 8))
+      (reverse-interleave (range 9) 3))))
+  (testing "range again"
+    (is (=
+      '((0 5) (1 6) (2 7) (3 8) (4 9))
+      (reverse-interleave (range 10) 5)))))
